147e8ad8494702349db6de9d76af85f5125443c2
ismailonur <ismailonur15@gmail.com>
2020-12-03 15:49:57 +0300

    native base paketi kuruldu

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
 
 export default class Login extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Login </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
 }
