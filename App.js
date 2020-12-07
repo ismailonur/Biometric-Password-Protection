853e9cdf08eb361dc8b1917967d494bd39c87b81
ismailonur <ismailonur15@gmail.com>
2020-12-07 18:14:40 +0300

    Giriş yapıldıktan sonra geri tuşu engellendi ve uygulamadan çıkış fonlsiyonları eklendi

--- App.js
+++ App.js
@@ -1,30 +1,44 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
 import Welcome from './src/screens/Welcome';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
 import Main from './src/screens/Main';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Main" component={Main} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} options={{
+          title: 'Overview',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Overview' }} />
+        <Stack.Screen name="Signup" component={Signup} options={{ title: 'Overview' }} />
+        <Stack.Screen name="Main" component={Main} options={{
+          title: 'Overview', 
+          headerLeft: false
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, BackHandler, Alert } from 'react-native'
 
 export default class Main extends Component {
+
+    onButtonPress = () => {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+        // then navigate
+        navigate('NewScreen');
+      }
+      
+      handleBackButton = () => {
+       Alert.alert(
+           'Uygulamadan Çık',
+           'Uygulamadan çıkılsın mı?', [{
+               text: 'Hayır',
+               onPress: () => console.log('Cancel Pressed'),
+               style: 'cancel'
+           }, {
+               text: 'Evet',
+               onPress: () => BackHandler.exitApp()
+           }, ], {
+               cancelable: false
+           }
+        )
+        return true;
+      } 
+      
+      componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+      }
+      
+      componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+      }
+
     render() {
         return (
             <View>
