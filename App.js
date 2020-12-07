e6c3aa68f9b016dc57d378936fee31c3c8f57815
ismailonur <ismailonur15@gmail.com>
2020-12-08 00:42:09 +0300

    Giriş kontrol yapıldı

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,10 +6,45 @@ import Welcome from './src/screens/Welcome';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
 import Main from './src/screens/Main';
+import auth from '@react-native-firebase/auth';
 
 const Stack = createStackNavigator();
 
 function App() {
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+  
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+      setUser(user);
+      if (initializing) setInitializing(false);
+    }
+  
+    useEffect(() => {
+      const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+      return subscriber; // unsubscribe on unmount
+    }, []);
+  
+    if (initializing) return null;
+
+    function checkUser(){
+      //auth().signOut();
+      let stacks;
+      if (user !== null) {
+        stacks =  <Stack.Screen name="Main" component={Main} options={{
+          title: 'Şifreler', 
+          headerLeft: false
+        }} />;
+        return stacks;
+      }
+
+      stacks = <Stack.Screen name={"Welcome"} component={Welcome} 
+      options={{ title: "hoş geldin"}} 
+     />
+      return stacks;
+    }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -23,15 +58,11 @@ function App() {
            },
          }}
       >
-        <Stack.Screen name="Welcome" component={Welcome} 
-         options={{ title: 'Hoşgeldiniz'}} 
-        />
+        
+        { checkUser() }
         <Stack.Screen name="Login" component={Login} options={{ title: 'Giriş Yap' }} />
         <Stack.Screen name="Signup" component={Signup} options={{ title: 'Kayıt Ol' }} />
-        <Stack.Screen name="Main" component={Main} options={{
-          title: 'Şifreler', 
-          headerLeft: false
-        }} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
