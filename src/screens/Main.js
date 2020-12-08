8ce477adbe9f58bf8b24d7992bac2cc16008487a
ismailonur <ismailonur15@gmail.com>
2020-12-08 12:35:07 +0300

    tab bar eklendi

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,44 +1,86 @@
-import React, { Component } from 'react'
+import * as React from 'react';
 import { Text, View, BackHandler, Alert } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default class Main extends Component {
+const Tab = createBottomTabNavigator();
+
+export default class Main extends React.Component {
+
+    HomeScreen() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
+    }
+
+    SettingsScreen() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+            </View>
+        );
+    }
 
     onButtonPress = () => {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
         // then navigate
         navigate('NewScreen');
-      }
-      
-      handleBackButton = () => {
-       Alert.alert(
-           'Uygulamadan Çık',
-           'Uygulamadan çıkılsın mı?', [{
-               text: 'Hayır',
-               onPress: () => console.log('Hayır Basıldı!'),
-               style: 'cancel'
-           }, {
-               text: 'Evet',
-               onPress: () => BackHandler.exitApp()
-           }, ], {
-               cancelable: false
-           }
+    }
+
+    handleBackButton = () => {
+        Alert.alert(
+            'Uygulamadan Çık',
+            'Uygulamadan çıkılsın mı?', [{
+                text: 'Hayır',
+                onPress: () => console.log('Hayır Basıldı!'),
+                style: 'cancel'
+            }, {
+                text: 'Evet',
+                onPress: () => BackHandler.exitApp()
+            },], {
+            cancelable: false
+        }
         )
         return true;
-      } 
-      
-      componentDidMount() {
+    }
+
+    componentDidMount() {
         BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
-      }
-      
-      componentWillUnmount() {
+    }
+
+    componentWillUnmount() {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
-      }
+    }
 
     render() {
         return (
-            <View>
-                <Text> Main </Text>
-            </View>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Home') {
+                            iconName = focused
+                                ? 'md-add'
+                                : 'md-add';
+                        } else if (route.name === 'Settings') {
+                            iconName = focused ? 'md-add' : 'md-add';
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'red',
+                    inactiveTintColor: 'gray',
+                }}
+            >
+                <Tab.Screen name="Home" component={this.HomeScreen} />
+                <Tab.Screen name="Settings" component={this.SettingsScreen} />
+            </Tab.Navigator>
         )
     }
 }
